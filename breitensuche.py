6b2cff9633f44958891c51d72d5b9ad3ab7eefb6
Erik Heinz <jerik@gmx.net>
2020-06-07 22:26:51 +0200

    breitensuchen with check for searched items

--- breitensuche.py
+++ breitensuche.py
@@ -5,9 +5,11 @@ logfile = sys.argv[0].replace('.py', '.log')
 logging.basicConfig(filename=logfile, level=logging.DEBUG)
 # logging.debug(f'Step {i}: {guess}')
 
+graph = {}
+
 
 def main():
-    graph = {}
+    global graph
     graph['du'] = ['alice', 'bob', 'claire']
     graph['bob'] = ['anuj', 'peggy']
     graph['alice'] = ['peggy']
@@ -16,20 +18,32 @@ def main():
     graph['peggy'] = []
     graph['tom'] = []
     graph['jonny'] = []
+    logging.debug(f'graph: {graph}')
 
+    search('du')
+
+
+def search(name):
+    global graph
     search_queue = deque()
-    search_queue += graph['du']
+    search_queue += graph[name]
+    searched = []
+    logging.debug(f'Start searching with: {name}')
 
     while search_queue:
         person = search_queue.popleft()
         logging.debug(f'Person: {person}')
-        if person_is_seller(person):
-            logging.debug(f'{person} ist mangohaendler')
-            print(person + " ist Mangohaendler!")
-            return True
-        else:
-            search_queue += graph[person]
-            logging.debug(f'added: graph[{person}]')
+        if person not in searched:
+            # import pdb; pdb.set_trace()
+            if person_is_seller(person):
+                logging.debug(f'{person} ist mangohaendler')
+                print(person + " ist Mangohaendler!")
+                return True
+            else:
+                search_queue += graph[person]
+                logging.debug(f'added: graph[{person}]')
+                searched.append(person)
+    return False
 
 
 def person_is_seller(name):
