ac0020d9058e58d7c5d156963b8318424c67b6aa
Erik Heinz <jerik@gmx.net>
2020-06-08 23:30:33 +0200

    played around with tuples

---
+++ tinker-around.py
@@ -0,0 +1,56 @@
+# see: https://github.com/egonschiele/grokking_algorithms
+import logging, sys
+import random
+logfile = sys.argv[0].replace('.py', '.log')
+logging.basicConfig(filename=logfile, level=logging.DEBUG)
+# logging.debug(f'Step {i}: {guess}')
+
+# running from vim
+# error with local...
+# :! export LANG="de_DE.UTF-8"; py %
+
+
+def main():
+    # tupel()
+    # tupelzuweisung()
+    # testthis()
+    zufall()
+
+
+def tupel():
+    tupel = 'a', 'b', 'c', 'd', 'e'
+    print(tupel)
+    # logging.debug(f'{tupel}')
+    tupell = ('a', 'b', 'c', 'd', 'e', 'f')
+    print(tupell)
+    tl = ('a',)
+    print(type(tl))
+    print(tupell[1:3])
+    tupell = ('A',) + tupell[1:]
+    print(tupell)
+
+
+def tupelzuweisung():
+    a, b = 1, 7
+    a, b, c = 1, 'zwerg', 7
+    # print(f'{a} und {b} macht {c}')
+
+
+def swap(x, y):
+    return y, x
+
+
+def testthis():
+    c = 'wirding'
+    a, b = swap(1, 2)
+    # print(f'{a} und {b} macht {c}')
+
+
+def zufall():
+    for i in range(10):
+        x = random.random()
+        print(x)
+
+
+if __name__ == '__main__':
+    main()
